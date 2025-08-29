5d0dd2ac929e53987a30609ab4c2cbc465406fc0
Clique33 <gabrielc@eng.uerj.br>
2025-08-29 10:36:54 -0300

    Gŕafico é alterado no eixo x, mas ainda falta fazê-lo escalar no y

--- nodes/line_chart.gd
+++ nodes/line_chart.gd
@@ -2,7 +2,13 @@ extends Node2D
 class_name Chart
 
 @export var values_per_time : Array[float]
-@export var initial_value : float = 1.0
+@export_range(0,1000000,0.01) var initial_value : float
+@export_range(1,100000) var number_of_time_points : int:
+	set(value):
+		number_of_time_points = value
+		if(get_viewport() == null): 
+			return
+		_pixels_per_time_tick = get_viewport_rect().size.x/float(number_of_time_points)
 
 @onready var stock_line: Line2D = $StockLine
 @onready var timer: Timer = $Timer
@@ -12,9 +18,18 @@ enum {LOST = 0, WON = 1}
 
 var _initial_x_offset : int
 var _initial_y_offset : int
-var _last_added_value : float
-var _pixels_per_time_tick : int = 30
-var _pixels_per_value_tick : int = -100
+var _pixels_per_time_tick : int
+var _pixels_per_value_tick : int
+var _max_visible_value : float
+
+var current_value : float:
+	set(value):
+		current_value = value
+		if(get_viewport() == null): 
+			return
+		if current_value >= 2*initial_value:
+			_pixels_per_value_tick = -(get_viewport_rect().size.y/_max_visible_value*2)
+			_max_visible_value *= 2
 
 func change_by_percentage(percentage : float ) -> void:
 	if(len(values_per_time) == 0):
@@ -24,18 +39,24 @@ func change_by_percentage(percentage : float ) -> void:
 	values_per_time.append(values_per_time[-1]*(1+percentage))
 	
 func _get_next_point() -> Vector2:
-	_last_added_value = values_per_time[stock_line.get_point_count()]
+	current_value = values_per_time[stock_line.get_point_count()]
 	var next_time = _initial_x_offset + _pixels_per_time_tick*stock_line.get_point_count()
-	var next_value = _initial_y_offset  + _pixels_per_value_tick*_last_added_value
+	var next_value = _initial_y_offset  + _pixels_per_value_tick*current_value
 	return Vector2(next_time,next_value)
 
 func _set_value_label(next_point : Vector2) -> void:
-	value_label.text = "R$%.2f" % _last_added_value
+	value_label.text = "R$%.2f" % current_value
 	value_label.position = Vector2(next_point)
 
 func _ready() -> void:
+	current_value = initial_value
+	_max_visible_value = 2*initial_value
+	if _pixels_per_time_tick == 0:
+		_pixels_per_time_tick = get_viewport_rect().size.x/float(number_of_time_points)
+	if _pixels_per_value_tick == 0:
+		_pixels_per_value_tick = -get_viewport_rect().size.y/float(2*initial_value)
 	_initial_x_offset = -get_viewport_rect().size.x/2
-	_initial_y_offset = 0
+	_initial_y_offset = get_viewport_rect().size.y/2
 
 func _physics_process(delta: float) -> void:
 	if stock_line.get_point_count() == len(values_per_time): 
@@ -45,7 +66,8 @@ func _physics_process(delta: float) -> void:
 	_set_value_label(next_point)
 
 func _on_timer_timeout() -> void:
+	print(_pixels_per_time_tick," ",_pixels_per_value_tick)
 	var modifier : int = 1
 	if(randi_range(LOST,WON)):
-		modifier = -1
+		modifier = 1
 	change_by_percentage(randi_range(20,51)*modifier)
