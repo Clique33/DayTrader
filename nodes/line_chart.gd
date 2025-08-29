34f5de764f2ed685927f28e95b928588950a2fb9
Clique33 <gabrielc@eng.uerj.br>
2025-08-29 15:55:43 -0300

    Gráfico corretamente escala de acordo com o valor

--- nodes/line_chart.gd
+++ nodes/line_chart.gd
@@ -18,8 +18,13 @@ enum {LOST = 0, WON = 1}
 
 var _initial_x_offset : int
 var _initial_y_offset : int
-var _pixels_per_time_tick : int
-var _pixels_per_value_tick : int
+var _pixels_per_time_tick : int = 0
+var _pixels_per_value_tick : int = 0:
+	set(value):
+		_pixels_per_value_tick = value
+		if not stock_line:
+			return
+		scale_values_from_chart()
 var _max_visible_value : float
 
 var current_value : float:
@@ -27,9 +32,17 @@ var current_value : float:
 		current_value = value
 		if(get_viewport() == null): 
 			return
-		if current_value >= 2*initial_value:
-			_pixels_per_value_tick = -(get_viewport_rect().size.y/_max_visible_value*2)
+		if current_value >= _max_visible_value:
+			_pixels_per_value_tick = -(get_viewport_rect().size.y/(_max_visible_value*2))
 			_max_visible_value *= 2
+
+func scale_values_from_chart() -> void:
+	print("scaled! max_value is ",_max_visible_value, ",  pixels_per_value is ", _pixels_per_value_tick, "and current value is ", current_value)
+	var point : Vector2
+	for i in stock_line.get_point_count():
+		point = stock_line.get_point_position(i)
+		point.y = _initial_y_offset  + _pixels_per_value_tick * values_per_time[i]
+		stock_line.set_point_position(i, point)
 
 func change_by_percentage(percentage : float ) -> void:
 	if(len(values_per_time) == 0):
@@ -49,25 +62,32 @@ func _set_value_label(next_point : Vector2) -> void:
 	value_label.position = Vector2(next_point)
 
 func _ready() -> void:
-	current_value = initial_value
 	_max_visible_value = 2*initial_value
 	if _pixels_per_time_tick == 0:
 		_pixels_per_time_tick = get_viewport_rect().size.x/float(number_of_time_points)
 	if _pixels_per_value_tick == 0:
 		_pixels_per_value_tick = -get_viewport_rect().size.y/float(2*initial_value)
+	print(_pixels_per_value_tick)
 	_initial_x_offset = -get_viewport_rect().size.x/2
 	_initial_y_offset = get_viewport_rect().size.y/2
+	current_value = initial_value
 
 func _physics_process(delta: float) -> void:
 	if stock_line.get_point_count() == len(values_per_time): 
 		return
+		
 	var next_point : Vector2 = _get_next_point()
 	stock_line.add_point(next_point)
 	_set_value_label(next_point)
 
+var counter_timeouts : int = 0
 func _on_timer_timeout() -> void:
-	print(_pixels_per_time_tick," ",_pixels_per_value_tick)
+	if counter_timeouts >= number_of_time_points:
+		print("stopped")
+		return
+	
+	counter_timeouts+=1
 	var modifier : int = 1
-	if(randi_range(LOST,WON)):
-		modifier = 1
-	change_by_percentage(randi_range(20,51)*modifier)
+	if(randf() < .33):
+		modifier = -1
+	change_by_percentage(randi_range(20,50)*modifier)
